4c1dd63ec06d93e4df9f52fec40effd7105337da
SowmyaArunachalam <106989392+SowmyaArunachalam@users.noreply.github.com>
2022-06-11 18:55:57 +0530

    Add files via upload

---
+++ Python/hello_world.py
@@ -0,0 +1,41 @@
+"""
+Here is a sample line of code that can be executed in Python:
+
+print("Hello, World!")
+You can just as easily store a string as a variable and then print it to stdout:
+
+my_string = "Hello, World!"
+print(my_string)
+The above code will print Hello, World! on your screen. Try it yourself in the editor below!
+
+Input Format
+
+You do not need to read any input in this challenge.
+
+Output Format
+
+Print Hello, World! to stdout.
+
+Sample Output 0
+
+Hello, World!
+"""
+def is_leap(year):
+    leap = False
+    if year%4==0:
+        if year%100==0:
+            if year%400==0:
+                return True
+            else:
+                return False
+        else :
+            return True
+    else :
+        return False
+    
+    # Write your logic here
+    
+    return leap
+
+year = int(input())
+print(is_leap(year))

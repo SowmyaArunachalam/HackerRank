df78218169a2d0a8d7a10e9bf525edaf12461091
Sowmya <106989392+SowmyaArunachalam@users.noreply.github.com>
2024-04-13 17:18:54 +0530

    Weather_Observation_Station_3.sql

---
+++ Mysql/Weather_Observation_Station_3.sql
@@ -0,0 +1,11 @@
+'''
+Query a list of CITY names from STATION for cities that have an even ID number. Print the results in any order, but exclude duplicates from the answer.
+The STATION table is described as follows:
+
+Station.jpg
+
+where LAT_N is the northern latitude and LONG_W is the western longitude.
+'''
+select DISTINCT city
+from station
+where mod(ID,2)=0;

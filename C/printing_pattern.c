37dc3340895e8d1847d80e14a78f189b96fdb0fd
SowmyaArunachalam <sowmyaarunachalam57@gmail.com>
2024-08-01 19:27:45 +0530

    Count the number of occurence of the given variable in a string

---
+++ C/printing_pattern.c
@@ -0,0 +1,93 @@
+    """_summary_
+    Print a pattern of numbers from  to  as shown below. Each of the numbers is separated by a single space.
+
+                            4 4 4 4 4 4 4  
+                            4 3 3 3 3 3 4   
+                            4 3 2 2 2 3 4   
+                            4 3 2 1 2 3 4   
+                            4 3 2 2 2 3 4   
+                            4 3 3 3 3 3 4   
+                            4 4 4 4 4 4 4   
+Input Format
+
+The input will contain a single integer .
+
+Constraints
+
+
+Sample Input 0
+
+2
+Sample Output 0
+
+2 2 2
+2 1 2
+2 2 2
+Sample Input 1
+
+5
+Sample Output 1
+
+5 5 5 5 5 5 5 5 5 
+5 4 4 4 4 4 4 4 5 
+5 4 3 3 3 3 3 4 5 
+5 4 3 2 2 2 3 4 5 
+5 4 3 2 1 2 3 4 5 
+5 4 3 2 2 2 3 4 5 
+5 4 3 3 3 3 3 4 5 
+5 4 4 4 4 4 4 4 5 
+5 5 5 5 5 5 5 5 5
+Sample Input 2
+
+7
+Sample Output 2
+
+7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 6 6 6 6 6 6 6 6 6 6 6 7 
+7 6 5 5 5 5 5 5 5 5 5 6 7 
+7 6 5 4 4 4 4 4 4 4 5 6 7 
+7 6 5 4 3 3 3 3 3 4 5 6 7 
+7 6 5 4 3 2 2 2 3 4 5 6 7 
+7 6 5 4 3 2 1 2 3 4 5 6 7 
+7 6 5 4 3 2 2 2 3 4 5 6 7 
+7 6 5 4 3 3 3 3 3 4 5 6 7 
+7 6 5 4 4 4 4 4 4 4 5 6 7 
+7 6 5 5 5 5 5 5 5 5 5 6 7 
+7 6 6 6 6 6 6 6 6 6 6 6 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 
+    
+    """
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+
+    int n;
+    scanf("%d", &n);
+  	int size=2*n-1;
+    int strt=0;
+    int end=size-1;
+    int num=n;
+    int arr[size][size];
+    while(strt<=end){
+    for(int i=strt;i<=end;i++){
+        for(int j=strt;j<=end;j++){
+            if(i==strt || j==strt || i==end ||j==end){
+            arr[i][j]=num;
+            }
+        }} 
+        ++strt;
+        --end;
+        --num;
+        
+    }
+    
+    for(int i=0;i<size;i++){
+        for(int j=0;j<size;j++){
+            printf("%d ",arr[i][j]);
+ } printf("\n");} 
+    return 0;
+}
